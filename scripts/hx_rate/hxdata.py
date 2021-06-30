97f4045d6e517d0c66bba468389b40d92d16caa6
Sugyan Dixit <dixitsugyan@gmail.com>
2021-06-30 16:20:24 -0500

    made a hxdata module

---
+++ scripts/hx_rate/hxdata.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data_from_hdx_ms_dist_(fpath):
+    """
+    get hdx mass distribution data
+    :param fpath: input .csv path for distribution
+    :return: timepoints, mass_distribution list
+    """
+    df = pd.read_csv(fpath)
+    time_points = np.asarray(df.columns.values[1:], dtype=float)
+    mass_distribution = df.iloc[:, 1:].values.T
+    return time_points, mass_distribution
