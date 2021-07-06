b6af7e2008ab7ab0b3e07728cac7a957ca4230d0
Sugyan Dixit <dixitsugyan@gmail.com>
2021-07-06 17:16:10 -0500

    use the sample.csv to generate the sample data

--- scripts/hx_rate/hxdata.py
+++ scripts/hx_rate/hxdata.py
@@ -19,12 +19,12 @@ def load_sample_data():
     gets the sample data from the workfoler/input_hx_dist/ folder
     :return: protein_name, protein_sequence, timepoints_list, mass_distribution array (2D)
     """
-    hx_ms_dist_fpath = '../../workfolder/input_hx_dist/HEEH_rd4_0097_hx_mass_dist.csv'
-    timepoints, mass_distribution = load_data_from_hdx_ms_dist_(hx_ms_dist_fpath)
     sample_fpath = '../../workfolder/sample.csv'
     sample_df = pd.read_csv(sample_fpath)
     prot_name = sample_df['name'].values[0]
     prot_seq = sample_df['sequence'].values[0]
+    hx_ms_dist_fpath = sample_df['hx_dist_fpath'].values[0]
+    timepoints, mass_distribution = load_data_from_hdx_ms_dist_(hx_ms_dist_fpath)
     return prot_name, prot_seq, timepoints, mass_distribution
 
 
