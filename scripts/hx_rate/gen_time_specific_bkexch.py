05f9121b7b3add8efa003257a1f50798664813ba
Sugyan Dixit <dixitsugyan@gmail.com>
2021-08-02 13:58:25 -0500

    fixed backexchange correction function. added a function do generate the mass rate object from file.

--- scripts/hx_rate/gen_time_specific_bkexch.py
+++ scripts/hx_rate/gen_time_specific_bkexch.py
@@ -54,8 +54,31 @@ class BackExchangeCorrection(object):
     """
     protein_names: list = None
     mass_rate_arr: np.ndarray = None
-    average_rate_arr: np.ndarray = None
-    correction_rate: np.ndarray = None
+    average_mass_rate_arr: np.ndarray = None
+
+    def get_average_rate(self):
+
+        if len(self.protein_names) == 1:
+            self.average_mass_rate_arr = self.mass_rate_arr[0]
+
+        else:
+            self.average_mass_rate_arr = np.average(self.mass_rate_arr, axis=0)
+
+
+def gen_corr_backexchange(mass_rate_array, fix_backexchange_value):
+    """
+
+    :param mass_rate_array:
+    :param fix_backexchange_value:
+    :return:
+    """
+    backexchange_array = np.zeros(len(mass_rate_array))
+
+    for ind, mass_rate in enumerate(mass_rate_array):
+        corr_bkexch = (((1 - fix_backexchange_value)/(-1 - 0)) * mass_rate) + fix_backexchange_value
+        backexchange_array[ind] = corr_bkexch
+
+    return backexchange_array
 
 
 def calc_mass_diff_rate(ref_mass, mass_array):
@@ -164,12 +187,12 @@ def gen_list_of_mass_rate_obj(sample_csv_fpath: str,
     return list_of_mass_rate_obj
 
 
-def filter_tp_bkexch_obj(list_of_tp_bkexch_obj, min_number_accept=1, ch_frac_threshold=0.02):
+def filter_mass_rate_object(list_of_mass_rate_obj, min_number_accept=1, ch_frac_threshold=0.02):
 
     # todo: add param description
 
-    accept_list = [x for x in list_of_tp_bkexch_obj if x.accept is True]
-    reject_list = [x for x in list_of_tp_bkexch_obj if x.accept is False]
+    accept_list = [x for x in list_of_mass_rate_obj if x.accept is True]
+    reject_list = [x for x in list_of_mass_rate_obj if x.accept is False]
 
     while len(accept_list) < min_number_accept:
         for ind, tp_bkexc_obj in enumerate(reject_list):
@@ -195,26 +218,30 @@ def generate_backexchange_correction_object(list_of_mass_rate_object):
 
     backexchange_corr_obj.mass_rate_arr = np.array([x.mass_rate_arr for x in accept_list])
 
-    if len(accept_list) == 1:
-        backexchange_corr_obj.average_rate_arr = accept_list[0].mass_rate_arr
-    else:
-        backexchange_corr_obj.average_rate_arr = np.average(backexchange_corr_obj.mass_rate_arr, axis=0)
-
-    backexchange_corr_obj.correction_rate = backexchange_corr_obj.average_rate_arr
-    backexchange_corr_obj.correction_rate[0] = backexchange_corr_obj.correction_rate[1]
+    backexchange_corr_obj.get_average_rate()
 
     return backexchange_corr_obj
 
 
-def gen_timepoint_backexchange_array(backexchange_value, correction_rate_array):
+def write_backexchange_corr_to_file(mass_rate_array, csv_out_path):
+    """
+    write the backexchange correction to a csv file with the correction rate
+    :param bakexchange_corr_obj: backexchange correction object
+    :param csv_out_path: csv output path
+    :return: None
+    """
 
-    # todo: add param description
+    # write the correction rate to csv file
 
-    backexchange_tp_ = np.zeros(len(correction_rate_array))
-    for ind, corr_rate in enumerate(correction_rate_array):
-        backexchange_tp_[ind] = backexchange_value + (corr_rate*backexchange_value)
+    header = 'idx,dM_rate\n'
+    data_string = ''
 
-    return backexchange_tp_
+    for ind, dm_rate in enumerate(mass_rate_array):
+        data_string += '{},{}\n'.format(ind, dm_rate)
+
+    with open(csv_out_path, 'w') as outfile:
+        outfile.write(header + data_string)
+        outfile.close()
 
 
 def plot_mass_rate_all(list_of_tp_bkexchange_obj, output_path):
@@ -269,37 +296,133 @@ def plot_mass_rate_all(list_of_tp_bkexchange_obj, output_path):
     plt.close()
 
 
-if __name__ == '__main__':
+def gen_backexchange_corr_obj_from_sample_list(sample_list_fpath: str,
+                                               rate_tol: float,
+                                               frac_threshold: float,
+                                               frac_threshold_bound: float,
+                                               start_bound: int,
+                                               end_bound: int,
+                                               max_rate: float,
+                                               min_number_paths: int,
+                                               change_frac_threshold: float,
+                                               plot_rate_path: str,
+                                               pickle_out_path: str,
+                                               csv_out_path: str,
+                                               return_flag: bool) -> object:
+    """
 
-    # protein_name_ = 'HEEH_rd4_0097'
-    # hx_ms_fpath_ = '/Users/smd4193/OneDrive - Northwestern University/hx_ratefit_gabe/hxratefit_new/lib15_ph7/HEEH_rd4_0097.pdb_9.65902_winner.cpickle.zlib.csv'
-    # rate_tol_ = 0.05
-    # frac_thres_ = .50
-    #
-    # tp_bkexch_obj = gen_tp_backexchange_obj_v2_from_file(protein_name=protein_name_,
-    #                                                      hx_ms_dist_fpath=hx_ms_fpath_,
-    #                                                      rate_tol=rate_tol_,
-    #                                                      frac_threshold=frac_thres_)
+    :param sample_list_fpath:
+    :param rate_tol:
+    :param frac_threshold:
+    :param frac_threshold_bound:
+    :param start_bound:
+    :param end_bound:
+    :param max_rate:
+    :param min_number_paths:
+    :param change_frac_threshold:
+    :param plot_rate_path:
+    :param pickle_out_path:
+    :param csv_out_path:
+    :param return_flag:
+    :return:
+    """
+
+    # generate a list of mass rate object from the sample list
+
+    list_of_mass_rate_obj = gen_list_of_mass_rate_obj(sample_csv_fpath=sample_list_fpath,
+                                                      rate_tol=rate_tol,
+                                                      frac_threshold=frac_threshold,
+                                                      frac_thres_ind=frac_threshold_bound,
+                                                      start_ind=start_bound,
+                                                      end_ind=end_bound,
+                                                      max_mass_rate=max_rate)
+
+    # filter the mass rate object
+    new_mass_rate_object_list = filter_mass_rate_object(list_of_mass_rate_obj=list_of_mass_rate_obj,
+                                                        min_number_accept=min_number_paths,
+                                                        ch_frac_threshold=change_frac_threshold)
+
+    # generate backexchange correction object from the new list
+    backexchange_corr_obj = generate_backexchange_correction_object(list_of_mass_rate_object=new_mass_rate_object_list)
+
+    if plot_rate_path is not None:
+        plot_mass_rate_all(list_of_tp_bkexchange_obj=new_mass_rate_object_list,
+                           output_path=plot_rate_path)
+
+    if pickle_out_path is not None:
+        write_pickle_object(obj=backexchange_corr_obj, filepath=pickle_out_path)
+
+    if csv_out_path is not None:
+        write_backexchange_corr_to_file(mass_rate_array=backexchange_corr_obj.average_mass_rate_arr, csv_out_path=csv_out_path)
+
+    if return_flag:
+        return backexchange_corr_obj
+
+
+if __name__ == '__main__':
 
     sample_fpath = '/Users/smd4193/OneDrive - Northwestern University/hx_ratefit_gabe/hxratefit_new/lib15_ph7_sample.csv'
 
-    list_of_tp_bkexch = gen_list_of_mass_rate_obj(sample_csv_fpath=sample_fpath,
-                                                  rate_tol=0.06,
-                                                  frac_threshold=0.5,
-                                                  frac_thres_ind=0.8,
-                                                  start_ind=1,
-                                                  end_ind=4,
-                                                  max_mass_rate=0.5)
+    # list_of_tp_bkexch = gen_list_of_mass_rate_obj(sample_csv_fpath=sample_fpath,
+    #                                               rate_tol=0.06,
+    #                                               frac_threshold=0.5,
+    #                                               frac_thres_ind=0.8,
+    #                                               start_ind=1,
+    #                                               end_ind=4,
+    #                                               max_mass_rate=0.5)
+    #
+    # new_list_ = filter_mass_rate_object(list_of_mass_rate_obj=list_of_tp_bkexch,
+    #                                  min_number_accept=2,
+    #                                  ch_frac_threshold=0.01)
+    #
+    # # plot_mass_rate_all(list_of_tp_bkexchange_obj=new_list_,
+    # #                    output_path=sample_fpath+'_massrate.pdf')
+    #
+    # bkexch_corr_obj = generate_backexchange_correction_object(list_of_mass_rate_object=new_list_)
+    #
+    # bkexch_value = .18
+    #
+    # bkexch_tp_1 = gen_corr_backexchange(mass_rate_array=bkexch_corr_obj.average_mass_rate_arr,
+    #                                     fix_backexchange_value=bkexch_value)
 
-    new_list_ = filter_tp_bkexch_obj(list_of_tp_bkexch_obj=list_of_tp_bkexch,
-                                     min_number_accept=2,
-                                     ch_frac_threshold=0.01)
+    corr_obj = gen_backexchange_corr_obj_from_sample_list(sample_list_fpath=sample_fpath,
+                                                          rate_tol=0.06,
+                                                          frac_threshold=0.5,
+                                                          frac_threshold_bound=0.8,
+                                                          start_bound=1,
+                                                          end_bound=4,
+                                                          max_rate=0.5,
+                                                          min_number_paths=2,
+                                                          change_frac_threshold=0.01,
+                                                          plot_rate_path=sample_fpath+'dm_rate.pdf',
+                                                          pickle_out_path=sample_fpath+'dm_rate.pickle',
+                                                          csv_out_path=sample_fpath+'dm_rate.csv',
+                                                          return_flag=True)
 
-    plot_mass_rate_all(list_of_tp_bkexchange_obj=new_list_,
-                       output_path=sample_fpath+'_massrate.pdf')
+    bkexch_value = 0.18
 
-    bkexch_corr_obj = generate_backexchange_correction_object(list_of_mass_rate_object=new_list_)
+    bkexch_corr = gen_corr_backexchange(mass_rate_array=corr_obj.average_mass_rate_arr,
+                                        fix_backexchange_value=bkexch_value)
 
-    bkexch_value = .18
-    bkexch_tp = gen_timepoint_backexchange_array(backexchange_value=bkexch_value,
-                                                 correction_rate_array=bkexch_corr_obj.correction_rate)
+    bkexch_corr_diff_rate = calc_mass_diff_rate(ref_mass=bkexch_corr[-1], mass_array=bkexch_corr)
+
+    from mpl_axes_aligner import align
+
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(len(corr_obj.average_mass_rate_arr)), corr_obj.average_mass_rate_arr, color='red')
+    ax.set_ylabel('dM_rate', color='red')
+
+    ax.set_xlabel('Timepoint index')
+
+    ax2 = ax.twinx()
+    ax2.plot(np.arange(len(bkexch_corr)), bkexch_corr, color='black')
+    ax2.set_ylabel('bkexchange', color='black')
+
+    align.yaxes(ax, 0, ax2, bkexch_value, 0.5)
+
+    plt.grid(axis='x')
+
+    plt.savefig(sample_fpath+'bkexch_corr.pdf')
+    plt.close()
+
+    print('heho')
